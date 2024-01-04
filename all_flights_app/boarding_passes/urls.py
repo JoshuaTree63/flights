aa6635a49c85ac80d8a5bccaac2b98c6994a6bcb
Joshua Levi <joshua.levi63@gmail.com>
2024-01-04 21:31:14 +0200

    add more to boarding_passes

--- all_flights_app/boarding_passes/urls.py
+++ all_flights_app/boarding_passes/urls.py
@@ -1,4 +1,13 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from all_flights_app.boarding_passes.v
+from all_flights_app.boarding_passes.views import BoardigPassesViewSet, download_boarding_pass
+
+router = DefaultRouter()
+router.register('', BoardigPassesViewSet)
+
+urlpatterns = router.urls
+urlpatterns.extend([
+    path('download', download_boarding_pass)
+])
+
