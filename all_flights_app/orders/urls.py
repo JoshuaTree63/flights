87f1baab966a3469498f7bef3e9f5d6ed407927f
Josh <Joshua.Levi63@gmail.com>
2024-01-10 19:29:58 +0200

    done with orders_urls

--- all_flights_app/orders/urls.py
+++ all_flights_app/orders/urls.py
@@ -1,2 +1,13 @@
 from rest_framework.routers import DefaultRouter
 from all_flights_app.orders.views import OrdersViewSet, calc_sum
+from django.urls import path
+
+router = DefaultRouter()
+router.register('', OrdersViewSet)
+
+urlpattens = [
+    path('calc',calc_sum)
+]
+
+urlpattens.extend(router.urls)
+
