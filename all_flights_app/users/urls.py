1de498d5276f6e297d0ae88457d7919f9e4a1ac1
Josh <Joshua.Levi63@gmail.com>
2024-01-14 22:20:55 +0200

    done with user_urls

--- all_flights_app/users/urls.py
+++ all_flights_app/users/urls.py
@@ -1,8 +1,26 @@
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-#
-# from rest_framework_simplejwt.views import (
-#     TokenObtainPairView,
-#     TokenRefreshView
-# )
-# from all_flights_app.users.views import
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
+from all_flights_app.users.views import UsersViewSet, me, ExtendedTokenObtainPairView, google_login, upload_profile_img,\
+    upload_profile_img_url, upload_profile_img_done, download_file
+
+router = DefaultRouter()
+router.register('', UsersViewSet)
+
+
+urlpatterns = [
+    path('tokens', ExtendedTokenObtainPairView.as_view()),
+    path('tokens/refresh', TokenRefreshView.as_view()),
+    path('me', me),
+    path('google-auth', google_login),
+    path('profile/img', upload_profile_img),
+    path('profile/img/presigned', upload_profile_img_url),
+    path('profile/img/done', upload_profile_img_done),
+    path('download', download_file),
+]
+
+urlpatterns.extend(router.urls)
